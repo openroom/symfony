c434dd61ff1a9e2ce918d770a01e8b1afc3e249b
Kushal Hada <kushaldeveloper@gmail.com>
2018-08-18 08:06:38 -0400

    add a new route using console
    [kus@localhost openroomsymfony]$ php bin/console make:controller
    
     Choose a name for your controller class (e.g. GrumpyPuppyController):
     > LuckyNumberController
    
     created: src/Controller/LuckyNumberController.php
     created: templates/lucky_number/index.html.twig
    
      Success!
    
     Next: Open your new controller class and add some pages!
    [kus@localhost openroomsymfony]$ php bin/console debug:router
     -------------------------- -------- -------- ------ -----------------------------------
      Name                       Method   Scheme   Host   Path
     -------------------------- -------- -------- ------ -----------------------------------
      lucky_number               ANY      ANY      ANY    /lucky/number
      _twig_error_test           ANY      ANY      ANY    /_error/{code}.{_format}
      _wdt                       ANY      ANY      ANY    /_wdt/{token}
      _profiler_home             ANY      ANY      ANY    /_profiler/
      _profiler_search           ANY      ANY      ANY    /_profiler/search
      _profiler_search_bar       ANY      ANY      ANY    /_profiler/search_bar
      _profiler_phpinfo          ANY      ANY      ANY    /_profiler/phpinfo
      _profiler_search_results   ANY      ANY      ANY    /_profiler/{token}/search/results
      _profiler_open_file        ANY      ANY      ANY    /_profiler/open
      _profiler                  ANY      ANY      ANY    /_profiler/{token}
      _profiler_router           ANY      ANY      ANY    /_profiler/{token}/router
      _profiler_exception        ANY      ANY      ANY    /_profiler/{token}/exception
      _profiler_exception_css    ANY      ANY      ANY    /_profiler/{token}/exception.css
     -------------------------- -------- -------- ------ -----------------------------------
    
    [kus@localhost openroomsymfony]$ composer require --dev symfony/profiler-pack
    Using version ^1.0 for symfony/profiler-pack
    ./composer.json has been updated
    Loading composer repositories with package information
    Updating dependencies (including require-dev)
    Nothing to install or update
    Writing lock file
    Generating autoload files
    ocramius/package-versions:  Generating version class...
    ocramius/package-versions: ...done generating version class
    Executing script cache:clear [OK]
    Executing script assets:install public [OK]
    
    [kus@localhost openroomsymfony]$ composer require --dev symfony/profiler-pack
    Using version ^1.0 for symfony/profiler-pack
    ./composer.json has been updated
    Loading composer repositories with package information
    Updating dependencies (including require-dev)
    Nothing to install or update
    Generating autoload files
    ocramius/package-versions:  Generating version class...
    ocramius/package-versions: ...done generating version class
    Executing script cache:clear [OK]
    Executing script assets:install public [OK]
    
    [kus@localhost openroomsymfony]$

--- public/index.php
+++ public/index.php
@@ -34,6 +34,6 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
 
 $kernel = new Kernel($env, $debug);
 $request = Request::createFromGlobals();
-$response = $kernel->handle($request);
+    $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
---
+++ src/Controller/LuckyNumberController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyNumberController extends AbstractController
+{
+    /**
+     * @Route("/lucky/number", name="lucky_number")
+     * @throws \Exception
+     */
+    public function index()
+    {
+        $number = random_int(0, 100);
+        return $this->render('lucky_number/index.html.twig', [
+            'controller_name' => 'LuckyNumberController',
+            'number' => $number,
+        ]);
+    }
+}
