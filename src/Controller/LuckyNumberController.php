ccda6c05f83e263bce82c0c61dfcf19ad3a18e66
Kushal Hada <kushaldeveloper@gmail.com>
2018-08-18 13:34:49 -0400

    add restriction to make get only
    User Deprecated: The "Sensio\Bundle\FrameworkExtraBundle\Configuration\Method" annotation is deprecated since version 5.2. Use "Symfony\Component\Routing\Annotation\Route" instead.
    Hide context    Show trace
    
    [▼
      "exception" => ErrorException {#359 ▶}
    ]

--- src/Controller/LuckyNumberController.php
+++ src/Controller/LuckyNumberController.php
@@ -4,11 +4,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class LuckyNumberController extends AbstractController
 {
     /**
      * @Route("/lucky/number", name="lucky_number")
+     * @Method("GET")
      * @throws \Exception
      */
     public function index()
